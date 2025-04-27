f8fce68cefafad19fc28dbae6105bb68d7677046
Francis Rosete <202312263@gordoncollege.edu.ph>
2025-04-27 14:44:51 +0800

    change ng progress name ver 2

--- PeakForm/resources/views/workout_plan_3.blade.php
+++ PeakForm/resources/views/workout_plan_3.blade.php
@@ -9,24 +9,20 @@
 <body>
   <div class="container">
     <h2>Let’s Build Your Personalized Plan!</h2>
-    <img src="images/logo_4.png" alt="Dumbbell Icon" class="icon" />
+    <img src="images/logo_4.png"  alt="Dumbbell Icon" class="icon" />
 
     <div class="form-box">
-      <form action="{{ route('workout_plan_4') }}" method="GET">
-        @csrf
-        <p class="question">Select workout intensity <br><span>(Select one)</span></p>
-
-        <input type="hidden" name="intensity" id="selectedIntensity" required />
-
-        <div class="goal-options">
-          <button type="button" class="goal-button" data-goal="High Intensity">High Intensity</button>
-          <button type="button" class="goal-button" data-goal="Moderate">Moderate</button>
-          <button type="button" class="goal-button" data-goal="Low Intensity">Low Intensity</button>
-        </div>
-
-        <button type="submit" class="proceed-button">Proceed</button>
-      </form>
+      <p class="question">Select workout intensity <br><span>(Select one)</span></p>
+      <div class="goal-options">
+        <button class="goal-button" data-goal="High Intensity">High Intensity</button>
+        <button class="goal-button" data-goal="Moderate">Moderate</button>
+        <button class="goal-button" data-goal="Low Intensity">Low Intensity</button>
+      </div>
     </div>
+
+    <a href="{{ route('workout_plan_4') }}">
+        <button class="proceed-button"> Proceed </button>
+    </a>
   </div>
 
   <script src="script.js"></script>
